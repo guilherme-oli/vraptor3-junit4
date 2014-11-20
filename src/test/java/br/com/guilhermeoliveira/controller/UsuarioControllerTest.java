29d8f36968bb24da386b34b5037c0145538b3ebf
guilherme.oliveira <guilherme.oliveira@NOTE-11-018676.futurekids.edu>
2014-11-20 18:04:42 -0200

    Adicionado Junit 4

---
+++ src/test/java/br/com/guilhermeoliveira/controller/UsuarioControllerTest.java
@@ -0,0 +1,57 @@
+package br.com.guilhermeoliveira.controller;
+
+import br.com.caelum.vraptor.util.test.MockResult;
+import br.com.guilhermeoliveira.service.UsuarioService;
+import br.com.guilhermeoliveira.util.EntityManagerFactoryProducer;
+import br.com.guilhermeoliveira.util.EntityManagerProducer;
+import javax.persistence.EntityManagerFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author guilherme.oliveira
+ */
+public class UsuarioControllerTest {
+    
+    private MockResult result;
+    private UsuarioController usuarioController;
+            
+    public UsuarioControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        result = new MockResult();
+        EntityManagerProducer em = new EntityManagerProducer(new EntityManagerFactoryProducer().getInstance());
+        UsuarioService usuarioService = new UsuarioService(em.getInstance());
+        usuarioController = new UsuarioController(result, usuarioService);
+    }
+    
+    @After
+    public void tearDown() {
+        usuarioController = null;
+        result = null;
+        System.gc();
+    }
+
+    @Test
+    public void testLista() {
+        System.out.println("lista");        
+        usuarioController.lista();
+        fail("The test case is a prototype.");
+    }
+    
+}
